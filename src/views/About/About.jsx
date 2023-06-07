a7c8c1f857988a88c7e3e97ab92bee181bf7bf1d
Jan Georg Henriksen <jangeorg85@gmail.com>
2023-06-07 19:16:36 +0200

    redigert om-delen noe

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,13 +1,17 @@
 import "./About.css";
 import ContentMargin from '../../hoc/ContentMargin';
+import Container from "../../hoc/Container";
 
 const About = () => {
   return (
     <ContentMargin>
+    <Container>
       <section className="About">
         <h2 className="About-h2 text-center">Om denne siden</h2>
-        <p>Dette er et bildegaller med en backend for enkel opplasting og sletting av bilder. Det var opprinnelig tenkt en veninne av meg, men lagde siden først i wordpress og så var den bra nok. Så dette er en enklere versjon bare, uten kontaktskjema.</p>
+        <p>Dette er et bildegalleri inkludert en backend for enkel opplasting og sletting av bilder. Bruker Amazon s3, sharp, blurhash for bildebehandling / opplasting.</p>
+        <p>Galleriet var opprinnelig tenkt som nettside til en kunstner jeg kjenner, men lagde siden først i wordpress og så var den bra nok. Så dette er en enklere / mer generell versjon jeg lagde etterpå bare, uten kontaktskjema.</p>
       </section>
+      </Container>
     </ContentMargin>
   );
 };
