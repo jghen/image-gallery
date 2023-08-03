dcf1f1a934fcce1ae5f0147d19a633912f518844
Jan Georg Henriksen <jangeorg85@gmail.com>
2023-08-03 12:31:57 +0200

    endret noe tekst

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <section className="About">
         <h2 className="About-h2 text-center">Om denne siden</h2>
         <p>Dette er et bildegalleri inkludert en backend for enkel opplasting og sletting av bilder. Bruker Amazon s3, sharp, blurhash, MySql og Sequelize.</p>
-        <p>Galleriet var opprinnelig tenkt som nettside til en kunstner jeg kjenner, men lagde siden først i wordpress og så var den bra nok. Så dette er en enklere / mer generell versjon jeg lagde etterpå bare, uten kontaktskjema.</p>
+        <p>Galleriet var opprinnelig tenkt som nettside til en kunstner jeg kjenner, men lagde siden først i wordpress og så var den bra nok. Så dette er en enklere / mer generell versjon jeg lagde etterpå bare.</p>
       </section>
       </Container>
     </ContentMargin>
