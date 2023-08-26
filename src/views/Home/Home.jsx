7c2498856a62ec8a0a06210a20bf71a565ca3fbe
Jan Georg Henriksen <jangeorg85@gmail.com>
2023-08-27 00:03:45 +0200

    trying blurhash with and height from parent component?

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,9 +3,6 @@ import Footer from "../../components/Footer/Footer";
 import ContentMargin from "../../hoc/ContentMargin";
 import { useNavigate } from "react-router-dom";
 import Container from "../../hoc/Container";
-import frog from '../../assets/green_frog.jpg'
-import { useSelector } from "react-redux";
-import { selectImages } from "../../components/Images/imagesSlice";
 import { Blurhash } from "react-blurhash";
 import { useState } from "react";
 
@@ -13,8 +10,6 @@ const Home = ({image}) => {
   
   //const
   const navigate = useNavigate();
-
-  console.log('home img',image)
 
   //state
   const [loaded, setLoaded] = useState(false);
@@ -31,10 +26,8 @@ const Home = ({image}) => {
       <div className="skeleton-home-img" style={{ display: loaded ? "none" : "block" }} >
         <Blurhash
           hash={image.blurHash}
-          width={250}
-          height={166}
-          resolutionX={40}
-          resolutionY={40}
+          resolutionX={50}
+          resolutionY={50}
           punch={1}
         />
       </div>
@@ -50,7 +43,6 @@ const Home = ({image}) => {
             style={loadedStyles}
             src={image.imageUrl}
             alt={image.title}
-            // loading={index > 6 ? "lazy" : "eager"}
             className="home-img"
             id={image.id}
             onLoad={imageLoaded}
@@ -60,12 +52,6 @@ const Home = ({image}) => {
           {loaded ? (
             <p>En samling nydelige verk. Denne siden har en backend for opplasting av bilder.</p>
             ) : <SkeletonText />}
-          {/* <img className="home-img" src={frog} alt="green frog" /> */}
-          {/* <h2 className="home-h2">Kunst - og flotte ting</h2> */}
-          {/* <p>
-            En samling nydelige verk. Denne siden har en backend for opplasting
-            av bilder.{" "}
-          </p> */}
           <button
             className="btn home-cta"
             onClick={goToImages}
